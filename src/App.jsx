96fea95b14c3112b8153f83f2b0b33c0ffcfd705
markuu-exe <marklawrenceamatong@gmail.com>
2026-04-21 01:49:19 +0800

    feat: add Profile pages for Requester and Runner with shared ProfileScreen component

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,11 @@ import ZoneOnboarding from './pages/Auth/07_Zone_Onboarding';
 import RequesterDashboard from './pages/Requester/02_Requester_Dashboard';
 import ActiveOrderRequester from './pages/Requester/05_Active_Order_Requester';
 import OrderHistoryRequester from './pages/Requester/08_Order_History_Requester';
+import ProfileRequester from './pages/Requester/09_Profile_Requester';
 import RunnerErrandBoard from './pages/Runner/03_Runner_Errand_Board';
 import ActiveOrderRunner from './pages/Runner/04_Active_Order_Runner';
 import OrderHistoryRunner from './pages/Runner/08_Order_History_Runner';
+import ProfileRunner from './pages/Runner/09_Profile_Runner';
 
 export default function App() {
   const { user, setUser, setLoading } = useAppStore();
@@ -46,9 +48,11 @@ export default function App() {
             <Route path="/" element={<RequesterDashboard />} />
             <Route path="/requester/active-order" element={<ActiveOrderRequester />} />
             <Route path="/requester/history" element={<OrderHistoryRequester />} />
+            <Route path="/requester/profile" element={<ProfileRequester />} />
             <Route path="/runner/board" element={<RunnerErrandBoard />} />
             <Route path="/runner/active-order" element={<ActiveOrderRunner />} />
             <Route path="/runner/history" element={<OrderHistoryRunner />} />
+            <Route path="/runner/profile" element={<ProfileRunner />} />
             <Route path="*" element={<Navigate to="/" />} />
           </>
         )}
