7e92992ff9251115abad2e6fef2bc3a4fa42f911
markuu-exe <marklawrenceamatong@gmail.com>
2026-04-25 09:12:17 +0800

    UI arbitraries cleanup: divider

--- src/components/ui/Divider.jsx
+++ src/components/ui/Divider.jsx
@@ -1,5 +1,3 @@
-import './Divider.css';
-
 export default function Divider({ className = '', ...props }) {
-  return <div className={`divider ${className}`} {...props} />;
+  return <div className={`h-px w-full bg-border-rule ${className}`} {...props} />;
 }
