c9619502533c705db636b6688d71899664398b5e
markuu-exe <marklawrenceamatong@gmail.com>
2026-05-04 15:23:51 +0800

    Wrap OrderHistoryRunner component in PageTransition for animated transitions

--- src/pages/Runner/08_Order_History_Runner.jsx
+++ src/pages/Runner/08_Order_History_Runner.jsx
@@ -1,5 +1,10 @@
 import OrderHistoryScreen from '@/components/shared/OrderHistoryScreen';
+import PageTransition from '@/components/shared/PageTransition';
 
 export default function OrderHistoryRunner() {
-	return <OrderHistoryScreen role="runner" />;
+	return (
+		<PageTransition>
+			<OrderHistoryScreen role="runner" />
+		</PageTransition>
+	);
 }
